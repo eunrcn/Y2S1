dcc74c547fe3e02207c5c9e8376cec871595c540
Eunice Koh <eunicekoh46@gmail.com>
2023-09-07 14:44:53 +0800

    after 2100

--- WK1-6/CS2100/labtut2/Lab2/lab2b.c
+++ WK1-6/CS2100/labtut2/Lab2/lab2b.c
@@ -31,53 +31,53 @@ adding a null at the end of the line */
   return 0;
 }
 
-// int hexVal(char hex) {
-//   switch (toupper(hex)) {
-//   case '0':
-//     return 0;
-//   case '1':
-//     return 1;
-//   case '2':
-//     return 2;
-//   case '3':
-//     return 3;
-//   case '4':
-//     return 4;
-//   case '5':
-//     return 5;
-//   case '6':
-//     return 6;
-//   case '7':
-//     return 7;
-//   case '8':
-//     return 8;
-//   case '9':
-//     return 9;
-//   case 'A':
-//     return 10;
-//   case 'B':
-//     return 11;
-//   case 'C':
-//     return 12;
-//   case 'D':
-//     return 13;
-//   case 'E':
-//     return 14;
-//   case 'F':
-//     return 15;
-//   }
-//   return 0;
-// }
-
 int hexVal(char hex) {
-    if (hex >= '0' && hex <= '9') {
-        return hex - '0';
-    }
-    if (hex >= 'A' && hex <= 'F') {
-        return hex - 'A' + 10;
-    }
-    return 0; // Default case
+  switch (toupper(hex)) {
+  case '0':
+    return 0;
+  case '1':
+    return 1;
+  case '2':
+    return 2;
+  case '3':
+    return 3;
+  case '4':
+    return 4;
+  case '5':
+    return 5;
+  case '6':
+    return 6;
+  case '7':
+    return 7;
+  case '8':
+    return 8;
+  case '9':
+    return 9;
+  case 'A':
+    return 10;
+  case 'B':
+    return 11;
+  case 'C':
+    return 12;
+  case 'D':
+    return 13;
+  case 'E':
+    return 14;
+  case 'F':
+    return 15;
+  }
+  return 0;
 }
+
+// int hexVal(char hex) {
+//     if (hex >= '0' && hex <= '9') {
+//         return hex - '0';
+//     }
+//     if (hex >= 'A' && hex <= 'F') {
+//         return hex - 'A' + 10;
+//     }
+//     return 0; // Default case
+// }
 
 
 int hexToDecimal(char hex[], size_t size) {
