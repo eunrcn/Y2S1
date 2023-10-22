9cfeec8bfb2d41619e9626eed0e29fa705268f1a
Eunice Koh <eunicekoh46@gmail.com>
2023-10-22 13:22:06 +0800

    level 2 bolie

--- WK1-6/pentago.py
+++ WK1-6/pentago.py
@@ -1,55 +1,81 @@
 import numpy as np
 
+
 def initialize_board():
     return np.zeros((6, 6), dtype=int)
+
 
 def display_board(board):
     for row in board:
         print(" ".join(map(str, row)))
 
+
 def apply_move(board, turn, row, col, rot):
-# Create a new board to avoid modifying the original
+    # Create a new board to avoid modifying the original
     new_board = board.copy()
 
     if new_board[row, col] == 0:
         new_board[row, col] = turn
         if np.any(new_board[3 * row:3 * row + 3, 3 * col:3 * col + 3] != 0):
-            new_board[3 * row:3 * row + 3, 3 * col:3 * col + 3] = np.rot90(new_board[3 * row:3 * row + 3, 3 * col:3 * col + 3], k=rot)
+            new_board[3 * row:3 * row + 3, 3 * col:3 * col + 3] = np.rot90(
+                new_board[3 * row:3 * row + 3, 3 * col:3 * col + 3], k=rot)
 
     return new_board
+
 
 def check_move(board, row, col):
     return board[row, col] == 0
 
-def computer_move(board, turn, level):
-    # Implement computer's move logic here based on the provided level
-    # For example, you can generate random valid moves for level 1.
-    # For more advanced levels, you can implement a smarter AI.
 
-    # For now, let's assume a simple random move for demonstration.
+def computer_move(board, turn, level):
+    if level == 2:
+        # Check for direct wins
+        for row in range(6):
+            for col in range(6):
+                if check_move(board, row, col):
+                    new_board = apply_move(board.copy(), turn, row, col, 0)
+                    if check_victory(new_board, turn) == turn:
+                        return row, col, 0
+
+        # Check for blocking opponent wins
+        opponent = 3 - turn
+        for row in range(6):
+            for col in range(6):
+                if check_move(board, row, col):
+                    new_board = apply_move(board.copy(), opponent, row, col, 0)
+                    if check_victory(new_board, opponent) == opponent:
+                        return row, col, 0
+
+    # If no direct wins or blocking moves, play a random valid move
     while True:
         row = np.random.randint(6)
         col = np.random.randint(6)
         if check_move(board, row, col):
             return row, col, np.random.randint(4)  # Random rotation
-        
+
 def display_board(board):
     for row in board:
         print(" ".join(map(str, row)))
+
 
 def check_victory(board, player, test_case=False):
     if test_case:
         # Check that you are not changing the input
         board_out = board.copy()
-    
+
     # Check for victory before the rotation
     for i in range(6):
         for j in range(6):
             if (
-                (i + 4 < 6 and np.all(board[i:i+5, j] == player)) or  # Check rows
-                (j + 4 < 6 and np.all(board[i, j:j+5] == player)) or  # Check columns
-                (i + 4 < 6 and j + 4 < 6 and np.all(np.diag(board[i:i+5, j:j+5]) == player)) or  # Check diagonal
-                (i + 4 < 6 and j - 4 >= 0 and np.all(np.diag(np.fliplr(board[i:i+5, j-4:j+1])) == player))  # Check anti-diagonal
+                # Check rows
+                (i + 4 < 6 and np.all(board[i:i+5, j] == player)) or
+                # Check columns
+                (j + 4 < 6 and np.all(board[i, j:j+5] == player)) or
+                # Check diagonal
+                (i + 4 < 6 and j + 4 < 6 and np.all(np.diag(board[i:i+5, j:j+5]) == player)) or
+                # Check anti-diagonal
+                (i + 4 < 6 and j - 4 >=
+                 0 and np.all(np.diag(np.fliplr(board[i:i+5, j-4:j+1])) == player))
             ):
                 return player  # Player wins
 
@@ -62,7 +88,8 @@ def check_victory(board, player, test_case=False):
                     (i + 4 < 6 and np.all(rotated_board[i:i+5, j] == player)) or
                     (j + 4 < 6 and np.all(rotated_board[i, j:j+5] == player)) or
                     (i + 4 < 6 and j + 4 < 6 and np.all(np.diag(rotated_board[i:i+5, j:j+5]) == player)) or
-                    (i + 4 < 6 and j - 4 >= 0 and np.all(np.diag(np.fliplr(rotated_board[i:i+5, j-4:j+1])) == player))
+                    (i + 4 < 6 and j - 4 >=
+                     0 and np.all(np.diag(np.fliplr(rotated_board[i:i+5, j-4:j+1])) == player))
                 ):
                     return player  # Player wins
 
@@ -74,9 +101,11 @@ def check_victory(board, player, test_case=False):
     # No winner yet
     return 0
 
+
 def is_draw(board):
     # Check if the board is full (no empty spaces)
     return np.all(board != 0)
+
 
 def display_board(board):
     # Create a horizontal border
@@ -106,6 +135,7 @@ def display_board(board):
 
     print("\n")
 
+
 def menu():
     board = initialize_board()
     player = 1
@@ -121,16 +151,18 @@ def menu():
                 col = int(input("Enter the column (0-5): "))
                 rot = int(input("Enter the rotation (0-3): "))
             except ValueError:
-                print("Invalid input. Please enter valid row, column, and rotation values.")
+                print(
+                    "Invalid input. Please enter valid row, column, and rotation values.")
                 continue
 
             if 0 <= row < 6 and 0 <= col < 6 and 0 <= rot < 4:
                 board = apply_move(board, player, row, col, rot)
             else:
-                print("Invalid input. Please enter valid row, column, and rotation values.")
+                print(
+                    "Invalid input. Please enter valid row, column, and rotation values.")
         else:
             print("Computer's turn:")
-            row, col, rot = computer_move(board, player, level=1)
+            row, col, rot = computer_move(board, player, level=2)
             print(f"Computer plays: row {row}, col {col}, rotation {rot}")
             board = apply_move(board, player, row, col, rot)
 
@@ -149,9 +181,6 @@ def menu():
             break
 
         player = 3 - player
-
-if __name__ == "__main__":
-    menu()
 
 
 if __name__ == "__main__":
