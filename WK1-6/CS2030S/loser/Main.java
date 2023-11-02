1f968ffa574f1de11bc87813c7841a2fb5014fcc
Eunice Koh <eunicekoh46@gmail.com>
2023-11-02 17:07:55 +0800

    yuh

--- WK1-6/CS2030S/loser/Main.java
+++ WK1-6/CS2030S/loser/Main.java
@@ -2,31 +2,18 @@ package loser;
 
 public class Main {
     public static void main(String[] args) {
-        // Code snippet 0
-        A a = A.of(1); // Create an instance of A with value 1
-        String result = a.toString(); // Call the toString method on the instance
-        System.out.println("Code snippet 0: " + result);
+        A a = A.of(1)
+            .flatMap(x -> incrA(x).flatMap(y -> sqrA(y)));
 
-        // Code snippet 1
-        String result1 = A.of(1).flatMap(x -> A.of(x)).toString();
-        System.out.println("Code snippet 1: " + result1);
+        String result = a.toString();
+        System.out.println("Result: " + result);
+    }
 
-        // Code snippet 2
-        String result2 = A.of(10).incrA(10).toString();
-        System.out.println("Code snippet 2: " + result2);
+    public static A incrA(int x) {
+        return new A(x + 1, x);
+    }
 
-        // Code snippet 3
-        String result3 = A.of(10).incrA(10).flatMap(x -> A.of(x)).toString();
-        System.out.println("Code snippet 3: " + result3);
-
-        // Code snippet 4
-        String result4 = A.of(1).flatMap(x -> A.of(x).flatMap(y -> A.of(y).sqrA(y))).toString();
-        System.out.println("Code snippet 4: " + result4);
-
-        // Code snippet 5
-        String result5 = A.of(1)
-            .flatMap(x -> A.of(x).flatMap(y -> A.of(y).sqrA(y)))
-            .toString();
-        System.out.println("Code snippet 5: " + result5);
+    public static A sqrA(int x) {
+        return new A(x * x, x);
     }
 }
